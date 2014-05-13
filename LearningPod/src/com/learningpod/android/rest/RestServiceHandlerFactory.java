e92c61d36abf7271cbfdc4d2d78ecb12c8afb2ec
Deepak Pradeep <deepak@deepak-To-be-filled-by-O-E-M.(none)>
2014-05-13 18:32:45 +0530

    added untracked files

--- LearningPod/src/com/learningpod/android/rest/RestServiceHandlerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.learningpod.android.rest;
-
-public class RestServiceHandlerFactory {
-
-	public static HttpRestServiceHandler getServiceHandler(){
-		return new ApacheHttpRestServiceHandler();
-	}
-}
